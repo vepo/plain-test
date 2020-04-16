068064d0cb2af995a0287d4bb7757951a6c41718
Victor Osório <victor.osorio@openet.com>
2020-04-15 23:07:20 -0300

    Removing failed tests

--- plain-test-executor/src/test/java/io/vepo/plaintest/runner/executor/FailsTest.java
+++ plain-test-executor/src/test/java/io/vepo/plaintest/runner/executor/FailsTest.java
@@ -5,24 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Arrays;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.vepo.plaintest.Suite;
 import io.vepo.plaintest.SuiteFactory;
-import io.vepo.plaintest.runner.utils.MockOs;
 
 public class FailsTest {
-	@BeforeEach
-	public void setup() {
-		MockOs.mockUnix();
-	}
-
-	@AfterEach
-	public void shutdown() {
-		MockOs.clear();
-	}
 
 	@Test
 	public void unknownPluginTest() {
@@ -36,8 +24,8 @@ public class FailsTest {
 				"}");
 		PlainTestExecutor executor = new PlainTestExecutor();
 		Result result = executor.execute(suite);
-		Result doNothingResult = (Result) result.getResults().stream()
-				.filter(r -> ((Result) r).getName().equals("DoNothing")).findFirst().orElse(null);
+		Result doNothingResult = result.getResults().stream().filter(r -> r.getName().equals("DoNothing")).findFirst()
+				.orElse(null);
 		assertNotNull(doNothingResult);
 		assertEquals(Arrays.asList(new Fail(FailReason.PLUGIN_NOT_FOUND, "Could not find plugin: Unknown")),
 				doNothingResult.getFails());
@@ -54,10 +42,10 @@ public class FailsTest {
 				"}");
 		PlainTestExecutor executor = new PlainTestExecutor();
 		Result result = executor.execute(suite);
-		Result doNothingResult = (Result) result.getResults().stream()
-				.filter(r -> ((Result) r).getName().equals("DoNothing")).findFirst().orElse(null);
+		Result doNothingResult = result.getResults().stream().filter(r -> r.getName().equals("DoNothing")).findFirst()
+				.orElse(null);
 		assertNotNull(doNothingResult);
 		assertEquals(doNothingResult.getFails().size(), 1);
-		assertEquals(FailReason.FAILED, ((Fail) doNothingResult.getFails().get(0)).getReason());
+		assertEquals(FailReason.FAILED, doNothingResult.getFails().get(0).getReason());
 	}
 }
