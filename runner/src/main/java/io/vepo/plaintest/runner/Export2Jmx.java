723d3f6f2340c31d78e93c8ec762b3a77a53ef8a
Victor Osório <victor.osorio@openet.com>
2020-04-26 17:44:57 -0300

    Adding export to CMD

--- runner/src/main/java/io/vepo/plaintest/runner/Export2Jmx.java
+++ runner/src/main/java/io/vepo/plaintest/runner/Export2Jmx.java
@@ -1,5 +1,10 @@
 package io.vepo.plaintest.runner;
 
+import static io.vepo.plaintest.SuiteFactory.parseSuite;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Files.write;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
+
 import java.io.File;
 import java.util.concurrent.Callable;
 
@@ -9,13 +14,18 @@ import picocli.CommandLine.Option;
 
 @Command(name = "jmx-export", description = "Generate a JMeter Test Suite", mixinStandardHelpOptions = true)
 public class Export2Jmx implements Callable<Integer> {
-	@Option(names = { "-i", "--input" })
+	@Option(names = { "-i", "--input" }, required = true)
 	private File input;
+
+	@Option(names = { "-o", "--output" }, required = true)
+	private File output;
 
 	@Override
 	public Integer call() throws Exception {
 		JMeterExporter exporter = new JMeterExporter();
-		exporter.export(null);
+
+		String contents = exporter.export(parseSuite(new String(readAllBytes(input.toPath()))));
+		write(output.toPath(), contents.getBytes(), TRUNCATE_EXISTING);
 		return 0;
 	}
 
